7060b295348fa4557bb1d6d164dc2078ff68b093
MarcoBorgess <marco.borges@branef.com.br>
2022-05-18 11:08:31 -0300

    finished homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,6 +15,9 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
+          constraints: BoxConstraints(
+            minHeight: 100.h,
+          ),
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
@@ -32,98 +35,251 @@ class _HomePageState extends State<HomePage> {
               tileMode: TileMode.mirror,
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 100.w,
-                  height: 45.h,
-                  //TODO: IMAGENS
-                ),
-                Column(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 100.w,
+                height: 50.h,
+                child: Stack(
                   children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Let's Get\nStarted",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 45.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 2.h),
-                      child: Text(
-                        "Connect with each other with chatting or calling. Enjoy safe and private texting",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.sp,
-                          height: 1.75,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8.h),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context)
-                              .pushReplacementNamed('/register');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                          onPrimary: const Color(0xff017c73),
-                          minimumSize: Size(double.infinity, 40.sp),
-                          textStyle: TextStyle(
-                            fontSize: 15.sp,
-                            color: const Color(0xff017c73),
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: const Text(
-                          'Join Now',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 2.h),
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            const TextSpan(
-                              text: 'Already have an account? ',
-                            ),
-                            TextSpan(
-                              text: 'Login',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
+                    Positioned(
+                      top: 22.5.h,
+                      left: 52.5.w,
+                      child: RotationTransition(
+                        turns: const AlwaysStoppedAnimation(-15 / 360),
+                        child: Container(
+                          width: 48.w,
+                          height: 48.w,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(16, 0, 0, 0),
+                                spreadRadius: 1,
                               ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () => Navigator.of(context)
-                                    .pushReplacementNamed('/login'),
-                            )
-                          ],
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12.sp,
-                            height: 1.75,
+                            ],
+                            color: Color(0xffFFE7AA),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/char2.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 27.5.h,
+                      child: RotationTransition(
+                        turns: const AlwaysStoppedAnimation(15 / 360),
+                        child: Container(
+                          width: 30.w,
+                          height: 30.w,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(16, 0, 0, 0),
+                                spreadRadius: 1,
+                              ),
+                            ],
+                            color: Color(0xffEAD4FF),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/char1.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 10.h,
+                      left: 15.w,
+                      child: RotationTransition(
+                        turns: const AlwaysStoppedAnimation(-15 / 360),
+                        child: Container(
+                          width: 25.w,
+                          height: 25.w,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(16, 0, 0, 0),
+                                spreadRadius: 1,
+                              ),
+                            ],
+                            color: Color(0xffFFDDDD),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/char3.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 5.h,
+                      left: 50.w,
+                      child: RotationTransition(
+                        turns: const AlwaysStoppedAnimation(5 / 360),
+                        child: Container(
+                          width: 20.w,
+                          height: 20.w,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(16, 0, 0, 0),
+                                spreadRadius: 1,
+                              ),
+                            ],
+                            color: Color(0xffE6D7B3),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/char4.png'),
+                              fit: BoxFit.fill,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 24, right: 24),
+                child: Column(
+                  children: [
+                    const HomeTitle(
+                      title: "Let's Get\nStarted",
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 2.h),
+                      child: const HomeSubTitle(
+                        subTitle:
+                            "Connect with each other with chatting or calling. Enjoy safe and private texting",
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 6.h),
+                      child: const RegisterButton(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 2.h),
+                      child: const LoginText(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LoginText extends StatelessWidget {
+  const LoginText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          const TextSpan(
+            text: 'Already have an account? ',
+          ),
+          TextSpan(
+            text: 'Login',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap =
+                  () => Navigator.of(context).pushReplacementNamed('/login'),
+          )
+        ],
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 12.sp,
+          height: 1.75,
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).pushReplacementNamed('/register');
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        onPrimary: const Color(0xff017c73),
+        minimumSize: Size(double.infinity, 40.sp),
+        textStyle: TextStyle(
+          fontSize: 15.sp,
+          color: const Color(0xff017c73),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: const Text(
+        'Join Now',
+      ),
+    );
+  }
+}
+
+class HomeTitle extends StatelessWidget {
+  final String? title;
+
+  const HomeTitle({
+    Key? key,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Text(
+        title.toString(),
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 45.sp,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeSubTitle extends StatelessWidget {
+  final String? subTitle;
+
+  const HomeSubTitle({Key? key, this.subTitle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      subTitle.toString(),
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 12.sp,
+        height: 1.75,
       ),
     );
   }
